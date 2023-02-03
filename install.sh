626f7fee51de1d9292adedac801f6e578bf43fbb
SNIAPA <wasilewskihuberts@gmail.com>
2023-02-03 21:54:37 +0100

    wallpaper

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 pushd "$(dirname "$0")"
-echo $(realpath i3)
-ln -s $(realpath i3) $(realpath ~/.config/i3) -s
 
+ln -s $(realpath i3) $(realpath ~/.config/) -s
+
+nitrogen --set-zoom-fill wallpaper.png 
