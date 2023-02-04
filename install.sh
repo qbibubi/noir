69ad2950e50a0f56ddcaea7645dc414c99a45692
SNIAPA <wasilewskihuberts@gmail.com>
2023-02-04 11:33:23 +0100

    progress

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ pushd "$(dirname "$0")"
 ln -s $(realpath i3) $(realpath ~/.config/) -s
 ln -s $(realpath picom) $(realpath ~/.config/) -s
 ln -s $(realpath polybar) $(realpath ~/.config/) -s
-
-nitrogen --set-zoom-fill wallpaper.png 
+ln -s $(realpath conky) $(realpath ~/.config/) -s
+ln -s $(realpath wallpapers) $(realpath ~/.config/) -s
