379e77233ee6e1eaa30ae92338e7965c5eef52e9
SNIAPA <wasilewskihuberts@gmail.com>
2023-02-04 11:59:05 +0100

    new better script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 pushd "$(dirname "$0")"
 
-ln -s $(realpath i3) $(realpath ~/.config/) -s
-ln -s $(realpath picom) $(realpath ~/.config/) -s
-ln -s $(realpath polybar) $(realpath ~/.config/) -s
-ln -s $(realpath conky) $(realpath ~/.config/) -s
-ln -s $(realpath wallpapers) $(realpath ~/.config/) -s
+configs=("i3" "picom" "polybar" "conky" "wallpapers")
+
+
+for config in "${configs[@]}"; do 
+  file -f $(realpath ~/".config/${config}") && echo "${config} config already exists" || ln -s $(realpath ./"${config}") $(realpath ~/".config/${config}")
+done
+
